9594cc912df9cccaedc84725b68537f7e89e90a0
Vinesh Gurram <vineshgurram@gmail.com>
2023-07-30 19:50:27 +0530

    added some about , project section.

--- scripts/common.js
+++ scripts/common.js
@@ -2,3 +2,14 @@ $(".close-btn").click(function () {
   $(this).toggleClass("active");
   $(".nav").toggleClass("active");
 });
+
+
+let introSectionImgContent = {
+  imgPath : "images/gmail.png",
+  imgAlt : "This is an Images.",
+}
+
+
+let introSectionImgTemplate = `<img src="${introSectionImgContent.imgPath}" alt="${introSectionImgContent.imgAlt}">`;
+
+document.querySelector(".about-section")
