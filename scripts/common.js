b031d716d3bac41d5677b10d72ae39befa2b04a7
Vinesh <vinesh.gurram@theadroit.in>
2023-08-23 19:42:08 +0530

    added projects folder and about content

--- scripts/common.js
+++ scripts/common.js
@@ -1,7 +1,6 @@
 $(".close-btn").click(function () {
   $(this).toggleClass("active");
   $(".nav").toggleClass("active");
-  $("body").toggleClass("hidden");
 });
 
 $(".nav li").click(function () {
--- scripts/projects.js
+++ scripts/projects.js
@@ -5,7 +5,7 @@ let projectContent = [
         projectImgAlternateText:'hii images is here',
         projectName:'Simple Registration Page',
         projectTec: ["HTML","CSS"],
-        projectLink:'https://developers.google.com/search/docs/crawling-indexing/special-tags'
+        projectLink:'projects/register-page/index.html'
     },
     {
         projectId: 2,
@@ -13,34 +13,53 @@ let projectContent = [
         projectImgAlternateText:'hii images is here',
         projectName:'Tribute Page',
         projectTec: ["HTML","CSS"],
-        projectLink:'https://developers.google.com/search/docs/crawling-indexing/special-tags'
+        projectLink:'projects/tribute-page/index.html'
     },
     {
         projectId: 3,
         projectImg: 'images/pro-thumb-1.jpg',
         projectImgAlternateText:'hii images is here',
         projectName:'Rock Paper Scissors Game',
-        projectTec: ["HTML", "CSS","JavaScript "],
+        projectTec: ["HTML", "CSS","JavaScript"],
+        projectLink:'https://developers.google.com/search/docs/crawling-indexing/special-tags'
+    }
+    ,
+    {
+        projectId: 4,
+        projectImg: 'images/pro-thumb-1.jpg',
+        projectImgAlternateText:'hii images is here',
+        projectName:'To do List',
+        projectTec: ["HTML", "CSS","JavaScript"],
+        projectLink:'https://developers.google.com/search/docs/crawling-indexing/special-tags'
+    }
+    ,
+    {
+        projectId: 5,
+        projectImg: 'images/pro-thumb-1.jpg',
+        projectImgAlternateText:'hii images is here',
+        projectName:'Counter App',
+        projectTec: ["HTML", "CSS","JavaScript"],
+        projectLink:'https://developers.google.com/search/docs/crawling-indexing/special-tags'
+    }
+    ,
+    {
+        projectId: 6,
+        projectImg: 'images/pro-thumb-1.jpg',
+        projectImgAlternateText:'hii images is here',
+        projectName:'Calculator',
+        projectTec: ["HTML", "CSS","JavaScript",],
         projectLink:'https://developers.google.com/search/docs/crawling-indexing/special-tags'
     }
 ];
-
-// let len = projectContent[0].projectTec
-// console.log(len.length);
-
-console.log()
 
 let projectBoxTemplate = "";
 
 for (let j = 0; j < projectContent.length; j++) {
     let tech = "";
-    
     for (let h = 0; h < projectContent[j].projectTec.length; h++) {   
         tech += '<button>'+ projectContent[j].projectTec[h] +'</button>';
     }
-
     projectBoxTemplate += '<div class="project-box"><a href="'+ projectContent[j].projectLink +'" target="_blank"><div class="project-img mb-25"><img src='+ projectContent[j].projectImg + ' alt="'+ projectContent[j].projectImgAlternateText +'"></div></a><div class="project-info"><a href="'+ projectContent[j].projectLink +'" target="_blank"><h4 class="project-name mb-25">' + projectContent[j].projectName + '</h4></a><div class="techology-name">'+ tech +'</div></div></div>';
-
 }
 
 document.querySelector(".project-wrap").innerHTML = projectBoxTemplate;
